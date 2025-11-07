ddfa589f72d7b62617941dd8f1b23076886a9552
richard <rhshadrach@gmail.com>
2025-11-06 20:24:58 -0500

    Shorten issues when necessary

--- ci/make_issues.py
+++ ci/make_issues.py
@@ -9,6 +9,8 @@ from pathlib import Path
 
 import pandas as pd
 
+GITHUB_ISSUE_LENGTH = 65000
+
 
 def get_commit_range(*, benchmarks: pd.DataFrame, sha: str) -> str:
     """Get commit range between a hash and the previous hash that has a benchmark.
@@ -30,7 +32,7 @@ def get_commit_range(*, benchmarks: pd.DataFrame, sha: str) -> str:
     return result
 
 
-def execute(cmd):
+def execute(cmd: str) -> str:
     response = subprocess.run(cmd, shell=True, capture_output=True, check=False)
     if response.returncode != 0:
         raise ValueError(f"{response.stdout.decode()}\n\n{response.stderr.decode()}")
@@ -58,6 +60,44 @@ def time_to_str(x: float) -> str:
     return result
 
 
+def make_body(
+    base_url: str,
+    commit_range: str,
+    benchmarks: pd.DataFrame,
+    sha: str,
+    shorten: bool = False,
+) -> str:
+    body = f"[Commit Range]({base_url + commit_range})"
+    body += "\n\n"
+    body += (
+        "Subsequent benchmarks may have skipped some commits. The link"
+        " above lists the commits that are"
+        " between the two benchmark runs where the regression was identified."
+        "\n\n"
+    )
+
+    regressions = benchmarks[benchmarks["sha"].eq(sha) & benchmarks["is_regression"]]
+    for _, regression in regressions.iterrows():
+        benchmark = regression["name"]
+        params = regression["params"]
+        base_url = "https://pandas-dev.github.io/asv-runner/#"
+        url = f"{base_url}{benchmark}"
+        abs_change = time_to_str(regression["abs_change"])
+        severity = f"{regression['pct_change']:0.3%} ({abs_change})"
+        body += f" - [ ] [{benchmark}]({url})"
+        if params == "" or shorten:
+            body += f" - {severity}\n"
+            continue
+        body += "\n"
+        params_list = list(params.split(", "))
+        params_suffix = "?p-" + "&p-".join(params_list)
+        url = f"{base_url}{benchmark}{params_suffix}"
+        url = urllib.parse.quote(url, safe="/:?=&#")
+        body += f"   - [ ] [{params}]({url}) - {severity}\n"
+    body += "\n"
+    return body
+
+
 def run(input_path: str | Path):
     if not isinstance(input_path, Path):
         input_path = Path(input_path)
@@ -82,36 +122,21 @@ def run(input_path: str | Path):
         title = f"Commit {sha}"
         base_url = "https://github.com/pandas-dev/pandas/compare/"
         commit_range = get_commit_range(benchmarks=benchmarks, sha=sha)
-        body = f"[Commit Range]({base_url + commit_range})"
-        body += "\n\n"
-        body += (
-            "Subsequent benchmarks may have skipped some commits. The link"
-            " above lists the commits that are"
-            " between the two benchmark runs where the regression was identified."
-            "\n\n"
-        )
 
-        regressions = benchmarks[
-            benchmarks["sha"].eq(sha) & benchmarks["is_regression"]
-        ]
-        for _, regression in regressions.iterrows():
-            benchmark = regression["name"]
-            params = regression["params"]
-            base_url = "https://pandas-dev.github.io/asv-runner/#"
-            url = f"{base_url}{benchmark}"
-            abs_change = time_to_str(regression["abs_change"])
-            severity = f"{regression['pct_change']:0.3%} ({abs_change})"
-            body += f" - [ ] [{benchmark}]({url})"
-            if params == "":
-                result += f" - {severity}\n"
-                continue
-            body += "\n"
-            params_list = list(params.split(", "))
-            params_suffix = "?p-" + "&p-".join(params_list)
-            url = f"{base_url}{benchmark}{params_suffix}"
-            url = urllib.parse.quote(url, safe="/:?=&#")
-            body += f"   - [ ] [{params}]({url}) - {severity}\n"
-        body += "\n"
+        body = make_body(
+            base_url=base_url,
+            commit_range=commit_range,
+            benchmarks=benchmarks,
+            sha=sha,
+        )
+        if len(body) >= GITHUB_ISSUE_LENGTH:
+            body = make_body(
+                base_url=base_url,
+                commit_range=commit_range,
+                benchmarks=benchmarks,
+                sha=sha,
+                shorten=True,
+            )
 
         cmd = (
             f"gh issue create"

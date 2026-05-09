fdc23a312e1afa8c6b7baddbf78d996e7bced307
Richard Shadrach <rhshadrach@gmail.com>
2026-05-09 09:59:34 -0400

    Fixup

--- ci/make_issues.py
+++ ci/make_issues.py
@@ -53,9 +53,9 @@ def time_to_str(x: float) -> str:
     is_negative = x < 0.0
     if x >= 1.0:
         result = f"{x:0.3f}s"
-    elif x >= 0.001:  # noqa: PLR2004
+    elif x >= 0.001:
         result = f"{x * 1000:0.3f}ms"
-    elif x >= 0.000001:  # noqa: PLR2004
+    elif x >= 0.000001:
         result = f"{x * (1000 ** 2):0.3f}us"
     else:
         result = f"{x * (1000 ** 3):0.3f}ns"
